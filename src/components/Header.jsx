49d4449c063e904ab41e632d696cabbb4c3e42b6
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-07-18 23:29:16 -0300

    Add header component style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import styles from './Header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
+
 
 export function Header() {
     return (
-        <strong className={styles.header}>Ignite Feed</strong>
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Ignite logo"/>
+            <strong>Ignite Feed</strong>
+        </header>
     );
 }
