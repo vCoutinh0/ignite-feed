945cf4ea6b40365995e677a918b203167b596fa2
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-08-22 15:34:30 -0300

    Refactored to TypeScript

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,29 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 import styles from './Post.module.css';
 import { Avatar } from './Avatar';
-import { Comment } from './Comment.jsx';
+import { Comment } from './Comment.js';
 
-export function Post({ author, publishedAt, content }) {
+interface Author {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string
+}
+
+interface PostPropos {
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostPropos) {
+
     const [comments, setComments] = useState([
         'Muito top!'
     ])
@@ -21,24 +39,24 @@ export function Post({ author, publishedAt, content }) {
         addSuffix: true
     });
 
-    function handleCreateNewComment() {
+    function handleCreateNewComment(event: FormEvent) {
         event.preventDefault()
 
         setComments([...comments, newCommentText]);
         setNewCommentText('');
     }
     
-    function handleNewCommentChange() {
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('');
 
         setNewCommentText(event.target.value);
     }
 
-    function handleNewCommentInvalid() {
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('Esse campo é obrigatório!');
     }
 
-    function deleteComment(commentToDelete) {
+    function deleteComment(commentToDelete: string) {
         const commentsWithoutDeletedOne = comments.filter(comment => {
             return comment !== commentToDelete;
         })
